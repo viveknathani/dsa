237616da9ccf62409326da474103b0deb7ece603
viveknathani <viveknathani2402@gmail.com>
2020-01-09 19:32:01 +0530

    added traversals

--- 03_tree/binarytree.cpp
+++ 03_tree/binarytree.cpp
@@ -14,16 +14,43 @@ treeNode* create(int value)
 {
   int x;
   treeNode* n=new treeNode;
-  n->data=value
+  n->data=value;
   n->left=n->right=NULL;
   return n;
 }
 
+void preorder(treeNode *root)
+{
+  if(root==NULL) { return; }
+  cout<<root->data<<" ";
+  preorder(root->left);
+  preorder(root->right);
+}
+
+void inorder(treeNode *root)
+{
+  if(root==NULL) { return; }
+  inorder(root->left);
+  cout<<root->data<<" ";
+  inorder(root->right);
+}
+
+void postorder(treeNode *root)
+{
+  if(root==NULL) { return; }
+  postorder(root->left);
+  postorder(root->right);
+  cout<<root->data<<" ";
+}
+
 int main()
 {
-  treeNode *root=create(1);
-  root->left=create(2);
-  root->right=create(3);
-  root->left->left=create(3);
+  treeNode *root=create(4);
+  root->left=create(5);
+  root->right=create(10);
+  root->left->left=create(7);
+  root->left->right=create(8);
+  postorder(root);
+  cout<<endl;
   return 0;
 }

27f1e6d1b4ea77ff891604dfd8fdfea6b27870f8
viveknathani <viveknathani2402@gmail.com>
2020-02-04 09:45:57 +0530

    binary search

---
+++ 08_search/binary.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+
+int binarySearch(int arr[], int left, int right, int x)
+{
+  if(right>=left)
+  {
+    int mid=left+(right-1)/2;
+
+    if(arr[mid]==x) { return mid; }
+
+    if(arr[mid]>x) { return binarySearch(arr, left, mid-1, x); }
+
+    return binarySearch(arr, mid+1, right, x);
+  }
+  return -1;
+}
+
+int main()
+{
+  int arr[] = { 2, 3, 4, 10, 40 };
+  int x = -2;
+  int n = sizeof(arr) / sizeof(arr[0]);
+  int result = binarySearch(arr, 0, n - 1, x);
+  (result==-1) ?  cout<<"Element does not exist."<<endl : cout<<"Element found at index : "<<result<<endl;
+  return 0;
+}
